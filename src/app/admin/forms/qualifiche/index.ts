148c78547c4eb46ae0fb28aae87df74d1e158878
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2021-04-17 11:51:43 +0200

    cosri and qualifiche with auto grid

--- src/app/admin/forms/qualifiche/index.ts
+++ src/app/admin/forms/qualifiche/index.ts
@@ -1,10 +1,48 @@
 import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
 
-import List from './list';
+import listGridMaker from '../lists/list';
 import Edit from './edit';
 
+const fields = [
+  {
+    type: 'reference',
+    source: 'id_ente',
+    sourceRef: 'denominazione',
+    reference: 'enti',
+    label: 'Ente',
+  },
+  {
+    type: 'reference',
+    source: 'id_risorsa',
+    sourceRef: 'cognome',
+    reference: 'risorse',
+    label: 'Risorsa',
+  },
+  {
+    type: 'text',
+    source: 'data_qualifica',
+    label: 'Data qualifica',
+  },
+  {
+    type: 'text',
+    source: 'modalita',
+    label: 'Modalita',
+  },
+  {
+    type: 'text',
+    source: 'responsabile',
+    label: 'Responsabile',
+  },
+];
+
+const param = {
+  hideId: false,
+  sourceList: fields,
+};
+const list = listGridMaker(param);
+
 const qualifiche = {
-  list: List,
+  list,
   edit: Edit.Edit,
   create: Edit.Create,
   icon: AccountBalanceIcon,
