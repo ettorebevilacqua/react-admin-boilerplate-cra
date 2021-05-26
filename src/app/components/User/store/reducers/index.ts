10def935a53aaa347282dcf6b53218b38e887d04
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2021-05-26 17:33:09 +0200

    storage implementation

---
+++ src/app/components/User/store/reducers/index.ts
@@ -0,0 +1,22 @@
+import { combineReducers } from 'redux';
+import { History } from 'history';
+import { RouterState, connectRouter } from 'connected-react-router';
+// import counterReducer from './counter';
+
+const rootReducer = (history: History) =>
+  combineReducers({
+    //   count: counterReducer,
+    router: connectRouter(history),
+  });
+
+export interface State {
+  //  count: number;
+  router: RouterState;
+}
+
+const createRootReducer = history =>
+  combineReducers({
+    router: connectRouter(history),
+  });
+
+export default createRootReducer;
