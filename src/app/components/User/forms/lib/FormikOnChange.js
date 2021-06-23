140de525ecae53d9823182557e58b8572c73f2f8
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2021-06-23 09:19:35 +0200

    add lib

---
+++ src/app/components/User/forms/lib/FormikOnChange.js
@@ -0,0 +1,46 @@
+import { useRef, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+import useDeepCompareEffect from 'use-deep-compare-effect';
+import { useFormikContext } from 'formik';
+
+function debounce(fn, wait) {
+  let t;
+  return function () {
+    clearTimeout(t);
+    t = setTimeout(() => fn.apply(this, arguments), wait);
+  };
+}
+
+const FormikOnChange = ({ delay, onChange }) => {
+  const { values } = useFormikContext();
+  const isFirstRun = useRef(true);
+  const debouncedOnChange = useCallback(_.debounce(onChange, delay), []);
+
+  useDeepCompareEffect(() => {
+    if (isFirstRun.current) {
+      isFirstRun.current = false;
+      onChange(values, true);
+      return;
+    }
+
+    if (delay > 0) {
+      debouncedOnChange(values);
+    } else {
+      onChange(values, isFirstRun.current);
+    }
+  }, [values]);
+
+  return null;
+};
+
+FormikOnChange.propTypes = {
+  delay: PropTypes.number, // ms
+  onChange: PropTypes.func.isRequired,
+};
+
+FormikOnChange.defaultProps = {
+  delay: 0,
+};
+
+export default FormikOnChange;
