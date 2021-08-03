31d3251c94b5c44250faa8ebe013fa8a83d35d6d
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2021-08-03 20:56:26 +0200

    add overlay for load state

---
+++ src/app/components/Layout/LoadingOverlay.tsx
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from 'react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import LoadingOverlayComp from 'react-loading-overlay';
+
+const LoadingOverlay = props => <LoadingOverlayComp {...props} />;
+
+export default LoadingOverlay;
