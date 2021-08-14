5005c4091f769657f9071409bc3ce92d8777597b
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2021-08-14 13:43:01 +0200

    add functions

--- src/utils/functional.js
+++ src/utils/functional.js
@@ -2,6 +2,15 @@ export const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
 export const setGetObj = (obj, key, val) => ((obj[key] = val), obj);
 export const setGetObjC = obj => key => val => setGetObj(obj, key, val);
 
+export const mapOrReduceOnKeys = obj => (mapper, accMapper) => {
+  const keys = Object.keys(obj);
+  const reducerMapper = (acc, key, index) => mapper(acc, obj[key], key, index);
+
+  return accMapper !== undefined
+    ? keys.reduce(reducerMapper, accMapper)
+    : keys.map((key, index) => mapper(obj[key], key, index));
+};
+
 export const handlePromise = (promise, onSucces, onError) =>
   promise
     .then(data => (onSucces ? onSucces(data) : [data, undefined]))
