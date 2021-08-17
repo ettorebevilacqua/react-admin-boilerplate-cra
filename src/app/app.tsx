4548d510386da3ff5e2eaf4b260235b5f3620216
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2021-08-17 09:23:25 +0200

    fix router logic

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,12 @@
 /**
  * index.tsx
- *
- * This is the entry file for the application, only setup and boilerplate
- * code.
+
+  auth with provate route
+  https://ui.dev/react-router-v5-protected-routes-authentication/
+
  */
 // DEBUG: App Root
 import * as React from 'react';
-import { Provider } from 'react-redux';
 
 // Use consistent styling
 import 'sanitize.css/sanitize.css';
@@ -16,70 +16,29 @@ import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
 import { GlobalStyle } from '../styles/global-styles';
 
 import { NotFoundPage } from './components/NotFoundPage/Loadable';
-import { useTranslation } from 'react-i18next';
-
-import { Helmet, HelmetProvider } from 'react-helmet-async';
-
-import store from '../store/configureStore';
-
-import { useSelector, useDispatch } from 'react-redux';
-import { initUser, userSelector } from 'app/slice/userSlice';
-
 import { HomePage } from './pages/HomePage/Loadable';
 import { FormsPage } from './pages/forms/Loadable';
-
 import { UserPage } from './pages/User/Loadable';
 import { UserMenu } from './pages/UserMenu/Loadable';
 // import { ThemeProvider } from 'styles/theme/ThemeProvider';
-import { Login } from 'app/admin/layout';
+
 import dataApi from './data';
 
 // BOOKROOT: ROOT App
 import dataStore from '../store/dataStore';
-import { useUserAuthSlice } from 'app/slice';
 
 // dataApi.userProvider.login('ettore@bevilacqua.com1', 'password1').then(data=>alert(data));
 
-initUser(store); // check if user is logged with present token
-
-const helmetContext = {};
-
-function AppBody(props: any) {
-  const { actions } = useUserAuthSlice(); // start up slices
-  const { id, isFetching, isSuccess, isError } = useSelector(userSelector);
-  return isFetching ? (
-    <h2>Loading</h2>
-  ) : !id ? (
-    <Login />
-  ) : (
-    <BrowserRouter>
+export function AppHome(props: any) {
+  return (
+    <>
       <Switch>
         <Route exact path="/app/user" component={UserPage} />
-        <Route path="/app/user" component={UserPage} />
-        <Route path="/app/user" component={FormsPage} />
-        <Redirect from="/app" to="/app/user" exact />
-      </Switch>
-    </BrowserRouter>
-  );
-}
 
-export function AppHome(props: any) {
-  const { i18n } = useTranslation();
-  return (
-    <Provider store={store}>
-      <HelmetProvider context={helmetContext}>
-        <Helmet
-          titleTemplate="%s - React Boilerplate"
-          defaultTitle="Smart"
-          htmlAttributes={{ lang: i18n.language }}
-        >
-          <meta name="description" content="Smart service" />
-        </Helmet>
-
-        <AppBody {...props} />
-
+        <Redirect from="/app" to="/app/user" />
+        <Route component={NotFoundPage} />
         <GlobalStyle />
-      </HelmetProvider>
-    </Provider>
+      </Switch>
+    </>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,9 +3,9 @@
  *
  * This is the entry file for the application, only setup and boilerplate
  * code.
- * 
- * for route read 
- * 
+ *
+ * for route read
+ *
   https://stackoverflow.com/questions/56711663/react-router-v5-0-nested-routes
  */
 
@@ -15,24 +15,114 @@ import 'react-app-polyfill/stable';
 import * as React from 'react';
 
 import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
+import { Provider, connect } from 'react-redux';
+import { compose } from 'redux';
+import store from '../store/configureStore';
+import { useTranslation } from 'react-i18next';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
+import {
+  initUser,
+  mapStateToPropsUser,
+  userSelector,
+} from 'app/slice/userSlice';
 
 import { AppHome } from './app';
 import AdminApp from './admin';
+import { Login } from 'app/admin/layout';
 import { NotFoundPage } from './components/NotFoundPage/Loadable';
+import { HomePage } from './pages/HomePage/Loadable';
+import { useUserAuthSlice } from 'app/slice';
 
-// import { ThemeProvider } from 'styles/theme/ThemeProvider';
+initUser(store); // check if user is logged with present token
+const MyComp = () => <h3>my comp</h3>;
+const Public = () => (
+  <>
+    <h3>Public</h3>
+    <Route path="/guest/:token" component={MyComp} />
+  </>
+);
+
+const Protected = () => <h3>Protected</h3>;
+
+function PrivateRoute({ children, isAuthenticated, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) => {
+        console.log('xxxxx', isAuthenticated);
+        return isAuthenticated === true ? children : <Login />;
+      }}
+    />
+  );
+}
+
+const AppRoute = ({ isAuthenticated }) => (
+  <>
+    <Route path="/guest" component={Public} />
+    <PrivateRoute path="/app" isAuthenticated={isAuthenticated}>
+      <Switch>
+        <Route path="/app" component={AppHome} />
+        <Route exact path="/admin" component={AdminApp} />
+
+        <Route component={NotFoundPage} />
+      </Switch>
+    </PrivateRoute>
+    <Route exact path="/" component={HomePage}>
+      <Redirect from="/" to="/app/user" />
+    </Route>
+  </>
+);
+
+function AppBody(props: any) {
+  const { actions } = useUserAuthSlice(); // start up slices
+  const {
+    id,
+    isFetching,
+    mustAuth,
+    isSuccess,
+    isError,
+  } = props.formProp.stateLoad;
+
+  React.useEffect(() => {
+    console.log('check auth ', isFetching, mustAuth, id);
+  }, [props.formProp.stateLoad]);
+
+  return !isFetching && !id && !mustAuth ? (
+    <h3>start</h3>
+  ) : isFetching ? (
+    <h2>Loading</h2>
+  ) : (
+    <BrowserRouter>
+      <Switch>
+        <AppRoute isAuthenticated={!!id && !mustAuth} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+// const { mapStateToProps, mapDispatchToProps } = mapToPropsUser;
+const withConnect = connect(mapStateToPropsUser);
+const AppBodyConnected = compose(withConnect)(AppBody);
+
+const helmetContext = {};
 
 export function App() {
+  const { i18n } = useTranslation();
   return (
     <React.Fragment>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/app" component={AppHome} />
-          <Route exact path="/admin" component={AdminApp} />
-          <Redirect from="/" to="/app/user" exact />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </BrowserRouter>
+      <Provider store={store}>
+        <HelmetProvider context={helmetContext}>
+          <Helmet
+            titleTemplate="%s - React Boilerplate"
+            defaultTitle="Smart"
+            htmlAttributes={{ lang: i18n.language }}
+          >
+            <meta name="description" content="Smart service" />
+          </Helmet>
+          <AppBodyConnected />
+        </HelmetProvider>
+      </Provider>
     </React.Fragment>
   );
 }
