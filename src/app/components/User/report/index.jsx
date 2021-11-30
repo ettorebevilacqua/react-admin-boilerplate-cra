fde9723a50a82e6b6e1ce7e0d0425380a0995ea0
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2021-12-01 00:39:49 +0100

    add question responce

--- src/app/components/User/report/index.jsx
+++ src/app/components/User/report/index.jsx
@@ -1,11 +1,13 @@
 import { ListPartecipanti } from './partecipantiRender';
 import { Soddisfazione } from './soddisfazione';
 import { GraphQuestion } from './graph';
+import { QuestionResponce } from 'questionResponce';
 
 const exported = {
   ListPartecipanti,
   Soddisfazione,
   GraphQuestion,
+  QuestionResponce,
 };
 
 export default exported;
