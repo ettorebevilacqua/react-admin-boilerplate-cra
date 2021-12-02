5fbfc66ca32f98557528d3a8a409d149a74fa0ee
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2021-12-02 11:55:06 +0100

    fix menu for open and close soddisfazione

--- src/app/components/User/forms/question/menuListSatisfaction.js
+++ src/app/components/User/forms/question/menuListSatisfaction.js
@@ -1,5 +1,6 @@
 export const menuSatisfaction = [
   { link: '/app/user/indagini', label: 'Crea indagine' },
   { link: '/app/user/indagini/list/open', label: 'Indagini aperte' },
-  { link: '/app/user/indagini/list/close', label: 'Report Annuale' },
+  { link: '/app/user/indagini/list/close', label: 'Indagini chiuse' },
+  { link: '/app/user/indagini/list/annoReport', label: 'Report Annuale' },
 ];
