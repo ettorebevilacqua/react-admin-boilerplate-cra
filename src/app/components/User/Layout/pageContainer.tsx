3cebd610c250d4ac48040990846e08c06aafb716
Ettore Bevilacqua <ettorebevilacqua@gmail.com>
2021-12-02 11:53:00 +0100

    add wrapper layot for base page

---
+++ src/app/components/User/Layout/pageContainer.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { elemStyle } from './stylesPage';
+import Typography from '@material-ui/core/Typography';
+import GridChilds from 'app/components/User/forms/component/gridChilds';
+import { setMenuList } from 'app/slice/layoutSlice';
+
+export function PageContainer({ title, titleComp, RenderTitle, Content, Footer, menu, ...rest }) {
+  const classes: any = elemStyle();
+  React.useEffect(() => setMenuList(menu as any) as any, []);
+  const _renderTitle = () => (
+    <div className={classes.paperTitle}>
+      <GridChilds justify="space-between" style={{ alignItems: 'center' }} view={[9, 3]}>
+        <div>
+          <Typography variant="h3" color="textSecondary">
+            Risposte partecipanti :
+          </Typography>
+        </div>
+      </GridChilds>
+    </div>
+  );
+
+  return (
+    <div className={classes.root}>
+      {titleComp ? <RenderTitle /> : _renderTitle()}
+      <Content {...rest} />
+      {Footer && <Footer />}
+    </div>
+  );
+}
---
+++ src/app/components/User/Layout/pageContainerLoader.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+const a=React.useEffect;
+// export a;
+/*
+import React from 'react';
+import { setMenuList } from 'app/slice/layoutSlice';
+import LoadSliceData from 'app/components/loader';
+
+export function PageContainer({ data }) {
+
+}
+
+export function loader({ loadProps, Page, ...rest}) {
+ React.useEffect(() => setMenuList(loadProps.menuList), []);
+  return <LoadSliceData />;
+}
+*/
